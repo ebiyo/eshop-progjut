932e563ea8d3e5644efea21de7d2d99309d894da
ebiyo <ariefridzki@gmail.com>
2025-02-21 16:27:07 +0700

    fix: fixing an issue raised by PMD (Unnecessary public modifier on ProductService methods)

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product update(Product product);
-    public Product findById(String id);
-    public boolean deleteProduct(String id);
+    Product create(Product product);
+    List<Product> findAll();
+    Product update(Product product);
+    Product findById(String id);
+    boolean deleteProduct(String id);
 }
